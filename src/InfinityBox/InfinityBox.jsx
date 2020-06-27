1d4d36f3f19d309db1e8b78d24b310a30ae3998d
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-27 12:10:07 -0400

    yay

---
+++ src/InfinityBox/InfinityBox.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./InfinityBox.css";
+
+class InfinityBox extends Component {
+  render() {
+    return (
+      <div className="infinityWrapper">
+        <div className="container">
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+          <div className="frame"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InfinityBox;
