c0206e4d17694380d18e85df09fe6a41b9e6b358
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-05 15:19:17 -0400

    Updates

---
+++ precache-manifest.ef0780f2405a62fdf8fa2adbebc4392e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38cda01a9a0072cded7d10d1bb3c4f0",
+    "url": "/InfinityBox/index.html"
+  },
+  {
+    "revision": "fc92441f3b4a879b278e",
+    "url": "/InfinityBox/static/css/main.f741c689.chunk.css"
+  },
+  {
+    "revision": "155b47a1ac07fe8776f9",
+    "url": "/InfinityBox/static/js/2.2e4b93a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/InfinityBox/static/js/2.2e4b93a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc92441f3b4a879b278e",
+    "url": "/InfinityBox/static/js/main.f38d62ff.chunk.js"
+  },
+  {
+    "revision": "0f497231f1d9f2de47cd",
+    "url": "/InfinityBox/static/js/runtime-main.cff1a189.js"
+  }
+]);
